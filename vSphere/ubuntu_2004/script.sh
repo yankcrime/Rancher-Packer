9345e8161b49f75e031b58600d6d7a1e7aa190ca
Nick Jones <nick@dischord.org>
2020-07-24 16:56:40 +0100

    Fix DHCP IP uniqueness on Ubuntu 20.04
    Don't resort to relying on the NIC's MAC address to ensure uniqueness,
    instead rely on Machine ID but ensure that it's updated in both places.

--- vSphere/ubuntu_2004/script.sh
+++ vSphere/ubuntu_2004/script.sh
@@ -1,22 +1,13 @@
+# Cleanup
+#
+apt-get -y autoremove
+apt-get -y clean
+
 # Reset the machine-id value. This has known to cause issues with DHCP
 #
-echo -n > /etc/machine-id
-
-# Fix netplan for DHCP
-#
-cat <<EOF > /etc/netplan/01-netcfg.yaml
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    ens192:
-      dhcp4: yes
-      dhcp-identifier: mac
-EOF
-
-# Remove 127.0.1.1 line from /etc/hosts
-#
-sed -i '/127.0.1.1/d' /etc/hosts
+truncate -s 0 /etc/machine-id
+rm /var/lib/dbus/machine-id
+ln -s /etc/machine-id /var/lib/dbus/machine-id
 
 # Reset any existing cloud-init state
 #
