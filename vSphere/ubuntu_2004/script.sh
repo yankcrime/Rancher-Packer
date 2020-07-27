5ea13913485efe12a8382cffc38e8553f41dc371
Nick Jones <nick@dischord.org>
2020-07-27 14:51:14 +0100

    Create a 2GB swapfile, reduce final image size

--- vSphere/ubuntu_2004/script.sh
+++ vSphere/ubuntu_2004/script.sh
@@ -1,7 +1,15 @@
-# Cleanup
+# Apply updates and cleanup Apt cache
 #
+apt-get update ; apt-get -y dist-upgrade
 apt-get -y autoremove
 apt-get -y clean
+
+# Create a swapfile
+#
+fallocate -l 2G /.swap
+chmod 0600 /.swap
+mkswap /.swap
+echo '/.swap    swap    swap    default    0   0' >> /etc/fstab
 
 # Reset the machine-id value. This has known to cause issues with DHCP
 #
@@ -12,3 +20,10 @@ ln -s /etc/machine-id /var/lib/dbus/machine-id
 # Reset any existing cloud-init state
 #
 cloud-init clean -s -l
+
+# Final clean up
+# Zero out the free space to save space in the final image
+#
+dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
+rm -f /EMPTY
+sync
